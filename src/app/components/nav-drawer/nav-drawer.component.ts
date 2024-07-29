d88f5da26cfbf7116cfd6097f2d44218e250e4d6
ClearTonic21 <cleartonicgames@gmail.com>
2024-07-29 16:01:17 -0600

    entry numbers

--- src/app/components/nav-drawer/nav-drawer.component.ts
+++ src/app/components/nav-drawer/nav-drawer.component.ts
@@ -7,12 +7,17 @@ import { DexoExpansionPanelComponent } from "../../lib/expansion-panel/expansion
 import { DexoHeaderComponent } from "../header/header.component";
 
 interface EntryItem {
-  index: number;
+  entryNumber: EntryNumber;
   imgIconSrc?: string;
-  entryNumber?: string;
   name?: string;
   header?: boolean;
   disabled?: boolean;
+}
+
+interface EntryNumber {
+  idNumber: number;
+  seperator?:  string;
+  altNumber?: number;
 }
 
 @Component({
@@ -20,8 +25,7 @@ interface EntryItem {
     standalone: true,
     templateUrl: './nav-drawer.component.html',
     styleUrl: './nav-drawer.component.scss',
-    imports: [IgxButtonModule, IgxIconModule, IgxNavigationDrawerModule, IgxNavDrawerItemDirective, IgxFlexDirective, DexoFooterComponent, DexViewPanelComponent, DexoExpansionPanelComponent, DexoHeaderComponent],
-    providers: [IgxExpansionPanelModule]
+    imports: [IgxButtonModule, IgxIconModule, IgxNavigationDrawerModule, IgxNavDrawerItemDirective, IgxFlexDirective, DexoFooterComponent, DexViewPanelComponent, DexoExpansionPanelComponent, DexoHeaderComponent]
 })
 export class DexoNavDrawerComponent  {
   @ViewChild(IgxNavigationDrawerComponent, { static: true }) public drawer!: IgxNavigationDrawerComponent;
@@ -30,34 +34,33 @@ export class DexoNavDrawerComponent  {
   public selected: number = 0;
 
   public entryItems: EntryItem[] = [
-    { index:  0,  imgIconSrc: `${DexoIcon.Image}`,  name: 'dexEntry', entryNumber:  '00'  },
-    {	index:	1,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'01'	},
-    {	index:	2,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'02'	},
-    {	index:	3,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'03'	},
-    {	index:	4,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'04'	},
-    {	index:	5,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'05'	},
-    {	index:	6,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'06'	},
-    {	index:	7,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'07'	},
-    {	index:	8,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'08'	},
-    {	index:	9,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'09'	},
-    {	index:	10,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'10'	},
-    {	index:	11,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'11'	},
-    {	index:	12,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'12'	},
-    {	index:	13,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'13'	},
-    {	index:	14,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'14'	},
-    {	index:	15,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'15'	},
-    {	index:	16,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'16'	},
-    {	index:	17,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'17'	},
-    {	index:	18,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'18'	},
-    {	index:	19,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'19'	},
-    {	index:	20,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'20'	},
-    {	index:	21,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'21'	},
-    {	index:	22,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'22'	},
-    {	index:	23,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'23'	},
-    {	index:	24,	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry',	entryNumber:	'24'	}
+    { entryNumber:  {idNumber: 0},    imgIconSrc: `${DexoIcon.Image}`,  name: 'dexEntry'  },
+    { entryNumber:  {idNumber: 1},    imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:  {idNumber: 2},    imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 3}, 	  imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 4}, 	  imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 5}, 	  imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 6}, 	  imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 7}, 	  imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 8}, 	  imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 9}, 	  imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 11}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 12}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 13}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 14}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 15}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 16}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 17}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 18}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 19}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 20}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 20}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 21}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 22}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	},
+    { entryNumber:	{idNumber: 23}, 	imgIconSrc:	`${DexoIcon.Image}`,	name:	'dexEntry'	}
   ];
 
-  public navigate(item: { index: number; }) {
-    this.selected = item.index;
+  public navigate(item: { entryNumber: { idNumber: number}}) {
+    this.selected = item.entryNumber.idNumber;
   }
 }
