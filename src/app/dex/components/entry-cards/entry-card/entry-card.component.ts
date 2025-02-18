99701a67ce88f1c9fba9067e619428e28e3662a3
ClearTonic21 <cleartonicgames@gmail.com>
2025-02-17 17:15:58 -0700

    basic setup in place

--- src/app/dex/components/entry-cards/entry-card/entry-card.component.ts
+++ src/app/dex/components/entry-cards/entry-card/entry-card.component.ts
@@ -3,17 +3,23 @@ import { EntryCardType } from '../../../../enums/EntryCardType';
 import { TextEntryCardComponent } from '../text-entry-card/text-entry-card.component';
 import { ImageEntryCardComponent } from '../image-entry-card/image-entry-card.component';
 import { ListEntryCardComponent } from "../list-entry-card/list-entry-card.component";
+import { IgxExpansionPanelModule, IgxIconModule } from 'igniteui-angular';
 
 @Component({
   selector: 'entry-card',
   standalone: true,
-  imports: [ImageEntryCardComponent, TextEntryCardComponent, ListEntryCardComponent],
+  imports: [IgxIconModule, IgxExpansionPanelModule, ImageEntryCardComponent, TextEntryCardComponent, ListEntryCardComponent],
   templateUrl: './entry-card.component.html',
   styleUrl: './entry-card.component.scss'
 })
 export class EntryCardComponent {
   @Input() public entryCard!: any;
-  @Input() public entryEditMode: boolean = false;
+  @Input() public cardEditMode: boolean = false;
+
+  public clickHeader(e: Event): void {
+    e.stopPropagation();
+    e.preventDefault();
+  }
 
   public isTextEntryCard(): boolean {
     return this.entryCard.entryCardType === EntryCardType.Text;
